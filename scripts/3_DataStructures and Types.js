eced106c7800b7a3d3b6e9955eb0692d4b8b9edd
Jayaprakash-1994 <50334367+Jayaprakash-1994@users.noreply.github.com>
2022-02-15 18:54:25 +0530

    DataStructures and Types

--- scripts/3_DataStructures and Types.js
+++ scripts/3_DataStructures and Types.js
@@ -9,8 +9,12 @@
 7. symbol (Dynamically procduces the ananymous unique value ). Immutable
 8. object mutable
 */
-
+//Non-Premitive Data Types.
+// Array
+// Objects
+// Functions
 //In Javascript all primitive types are immutable. and Non-Primitive types are Mutable.
+//Non-primitive data types cannot be compared by value. Even if two non-primitive data types have the same properties and values, they are not strictly equal.
 //Boolean
 let bool = true;
 console.log(bool);
@@ -19,14 +23,48 @@ var nullvariable = null;
 //undefined
 var un_defined = undefined;
 //number
-var num  =  12;
+var num  =  "12";
+//Methods to Convert String to Integer and Float 
+//Converting String to Integer
+var integer = parseInt(num);
+var parseFloat =  parseFloat(num);
 //string
 var str = "Jayaprakash";
 //String Concationation.
 console.log(`${str} is Working at Quess Corperation`);
+
+//Long String Literals
+var longStringLiterals = 'asdsadasdsa asdsd \
+asdasdsadasdasd \
+'
 //Object
 var obj = {
     name: "Jayaprakash",
     age : 27,
     gender : "Male"
 }
+
+console.log(`Name is ${obj.name} and his age is ${obj.age}`);
+//String Methods and Properties :-
+//The string length method returns the number of characters in a string included empty space.
+str.length;
+//.toUpperCase()
+//.toLowerCase()
+//.subString() --> Takes 2 Parameter as Argument the starting index and number of characters to slice.
+//.split()  --> The split method splits a string at a specified place.
+//.trim()    --> Removes trailing space in the beginning or the end of a string.
+//.includes(): It takes a substring argument and it checks if substring argument exists in the string. includes() returns a boolean. If a substring exist in a string, it returns true, otherwise it returns false.
+//.replace(): takes as a parameter the old substring and a new substring.
+//.charAt(): Takes index and it returns the value at that index.
+//indexOf(): Takes a substring and if the substring exists in a string it returns the first position of the substring if does not exist it returns -1
+str.indexOf("Jayaprakash");
+//.lastIndexOf(): Takes a substring and if the substring exists in a string it returns the last position of the substring if it does not exist it returns -1
+//.concat(): it takes many substrings and joins them.
+//.startsWith(): it takes a substring as an argument and it checks if the string starts with that specified substring. It returns a boolean(true or false).
+//.endsWith(): it takes a substring as an argument and it checks if the string ends with that specified substring. It returns a boolean(true or false).
+//.search() it takes a substring as an argument and it returns the index of the first match. The search value can be a string or a regular expression pattern.
+//match: it takes a substring or regular expression pattern as an argument and it returns an array if there is match if not it returns null. Let us see how a regular expression pattern looks like. It starts with / sign and ends with / sign.
+let string = 'love'
+let patternOne = /love/     // with out any flag
+let patternTwo = /love/gi   // g-means to search in the whole text, i - case insensitive
+//.repeat(): it takes a number as argument and it returns the repeated version of the string.
