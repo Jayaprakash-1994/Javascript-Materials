416c580fbea56ba4e84aaf1de6dcb5bd4f523980
Jayaprakash-1994 <50334367+Jayaprakash-1994@users.noreply.github.com>
2022-02-15 19:01:22 +0530

    Literals

---
+++ scripts/4_literals.js
@@ -0,0 +1,19 @@
+//Literals.
+//Literals represents the values in javascript.These are fixed values that you provide in the Variable
+//7 Types of Literals.
+//1. Array Literals.
+var arr = [1,2,3,4,5];
+//2. Boolean Literals 
+var bool = true;
+//3.Floating Point Literals.
+var floa = Math.PI;
+//4. Numeric literals
+var num = 12;
+//5.Object Literals.
+var obj = {
+    name: "Jayaprakash"
+}
+//6.Regex Expersion literals
+var expersion = "\s.*";
+//7.String literals.
+var str = "String Literals are sequence of Characters";
