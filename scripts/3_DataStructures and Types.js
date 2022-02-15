7816da4369c62d410567bba42bba70c43e560591
Jayaprakash-1994 <50334367+Jayaprakash-1994@users.noreply.github.com>
2022-02-15 16:39:20 +0530

    Data Structures and Types

---
+++ scripts/3_DataStructures and Types.js
@@ -0,0 +1,32 @@
+//JavaScript Has 8 different Datatypes.
+/* 
+1. Boolean (true or false) immutable
+2. null (A Special keyword denoting 'null' value null is not same as Null and NULL) immutable
+3. undefined (Top level property whose value is not defined) immutable
+4. number (An Integer or floating point Numbers 42 or 12.3243) immutable
+5. bigint (An Integer with Arbitrary Position). immutable
+6. String (A Sequence of Character that represent text value). Immutable
+7. symbol (Dynamically procduces the ananymous unique value ). Immutable
+8. object mutable
+*/
+
+//In Javascript all primitive types are immutable. and Non-Primitive types are Mutable.
+//Boolean
+let bool = true;
+console.log(bool);
+//null
+var nullvariable = null;
+//undefined
+var un_defined = undefined;
+//number
+var num  =  12;
+//string
+var str = "Jayaprakash";
+//String Concationation.
+console.log(`${str} is Working at Quess Corperation`);
+//Object
+var obj = {
+    name: "Jayaprakash",
+    age : 27,
+    gender : "Male"
+}
